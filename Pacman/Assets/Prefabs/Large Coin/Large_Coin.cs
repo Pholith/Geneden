69ff15a658b417f2d34ce2bdca69a49ef7b7a6aa
bagurb <bastien.bagur@edu.esiee.fr>
2022-09-04 21:21:44 -0400

    Système de coin, Score dynamique, reset de game

---
+++ Pacman/Assets/Prefabs/Large Coin/Large_Coin.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Large_Coin : Coin
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        value = 100;
+    }
+
+}
