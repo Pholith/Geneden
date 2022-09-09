ab2cf4c7fbcca48d9ccb2e829e9c27042111cc12
aktoun <antoine_man@hotmail.com>
2022-09-08 21:24:19 -0400

    Déplacement basique Pacman

--- Pacman/Assets/Prefabs/Pacman/PacmanController.cs
+++ Pacman/Assets/Prefabs/Pacman/PacmanController.cs
@@ -1,45 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PacmanController : MonoBehaviour
 {
+    public float moveSpeed;
+    public Rigidbody2D rbPacman;
+    private Vector3 velocity = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    private const float DEFAULT_COULDOWN_SPEED = 0.1f; // More it is big, more it is slow
-    private float couldownBeforeNextMove = DEFAULT_COULDOWN_SPEED;
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        couldownBeforeNextMove -= Time.deltaTime;
-        if (couldownBeforeNextMove <= 0)
-        {
-            if (Input.GetKey(KeyCode.Z))
-            {
-                gameObject.transform.Translate(Vector2.up);
-                couldownBeforeNextMove = DEFAULT_COULDOWN_SPEED;
-            }
-            else if (Input.GetKey(KeyCode.Q))
-            {
-                gameObject.transform.Translate(Vector2.left);
-                couldownBeforeNextMove = DEFAULT_COULDOWN_SPEED;
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                gameObject.transform.Translate(Vector2.down);
-                couldownBeforeNextMove = DEFAULT_COULDOWN_SPEED;
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                gameObject.transform.Translate(Vector2.right);
-                couldownBeforeNextMove = DEFAULT_COULDOWN_SPEED;
-            }
-
-        }
-
+        float movementHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float movementVertical = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        MovePlayer(movementHorizontal, movementVertical);
     }
+    void MovePlayer(float _movementHorizontal, float _movementVertical)
+    {
+        //_movement = Mathf.Abs(_movement);
+        Vector3 targetVelocity = new Vector2(_movementHorizontal, rbPacman.velocity.y);
+        rbPacman.velocity = Vector3.SmoothDamp(rbPacman.velocity, targetVelocity, ref velocity, .05f);
+     
+        targetVelocity = new Vector2(rbPacman.velocity.x, _movementVertical);
+        rbPacman.velocity = Vector3.SmoothDamp(rbPacman.velocity, targetVelocity, ref velocity, .05f);
+    }
+
+    //Input.GetKey(KeyCode.Z);
 }
+
