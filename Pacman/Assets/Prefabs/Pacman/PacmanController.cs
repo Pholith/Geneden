fefbdacdbca9dd4565dea7ea8991de23387d1014
aktoun <antoine_man@hotmail.com>
2022-09-11 12:09:05 -0400

    Déplacement Pacman

--- Pacman/Assets/Prefabs/Pacman/PacmanController.cs
+++ Pacman/Assets/Prefabs/Pacman/PacmanController.cs
@@ -2,32 +2,80 @@ using UnityEngine;
 
 public class PacmanController : MonoBehaviour
 {
-    public float moveSpeed;
-    public Rigidbody2D rbPacman;
-    private Vector3 velocity = Vector3.zero;
+    public Rigidbody2D RbPacman;
+    public float speed;
+    public float speedMultiplier;
 
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 initialDirection;
+    public LayerMask obstacle;
+    public Vector2 Direction;
+    public Vector2 NextDirection;
+    public Vector3 InitialPosition;
+
+
+    private void Awake()
+    {
+        this.RbPacman = GetComponent<Rigidbody2D>();
+        //print(this.StartintPosition);
+    }
+
+    private void Start()
+    {
+        // Récupération de la position locale du Pacman
+        this.InitialPosition = this.transform.localPosition;
+
+        // Initialisation
+        this.speedMultiplier = 1.0f;
+        this.Direction = this.initialDirection;
+        this.NextDirection = Vector2.zero;
+        this.transform.localPosition = this.InitialPosition;
+        this.RbPacman.isKinematic = false;
+        this.enabled = true;
+    }
+
+    void Update()
     {
         
     }
 
     void FixedUpdate()
     {
-        float movementHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float movementVertical = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        MovePlayer(movementHorizontal, movementVertical);
+        // Tester la nouvelle direction tout le temps
+        if (this.NextDirection != Vector2.zero)
+        {
+            SetDir(this.NextDirection);
+        }
+
+        Vector2 position = this.RbPacman.position;
+        Vector2 translation = this.Direction * this.speed * this.speedMultiplier * Time.fixedDeltaTime;
+        Vector2 movement = position + translation;
+
+        this.RbPacman.MovePosition(movement);
     }
-    void MovePlayer(float _movementHorizontal, float _movementVertical)
+
+    public void SetDir(Vector2 dir)
     {
-        //_movement = Mathf.Abs(_movement);
-        Vector3 targetVelocity = new Vector2(_movementHorizontal, rbPacman.velocity.y);
-        rbPacman.velocity = Vector3.SmoothDamp(rbPacman.velocity, targetVelocity, ref velocity, .05f);
-     
-        targetVelocity = new Vector2(rbPacman.velocity.x, _movementVertical);
-        rbPacman.velocity = Vector3.SmoothDamp(rbPacman.velocity, targetVelocity, ref velocity, .05f);
+        //print(IsObstacle(direction));
+
+        // Vérifier s'il y a un mur dans la direction soumise
+        // Si c'est pas un obstacle
+        if (!IsWall(dir))
+        {
+            this.Direction = dir;
+            this.NextDirection = Vector2.zero;
+        }
+        // Si c'est un obstacle
+        else
+        {
+            this.NextDirection = dir;
+        }
     }
 
-    //Input.GetKey(KeyCode.Z);
-}
+    public bool IsWall(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(this.transform.localPosition, Vector2.one * 0.75f, 0.0f, direction, 1.5f, this.obstacle);
+        //Physics2D.BoxCast(this.transform.localPosition, Vector2.one * 0.75f, 0.0f, direction, 1.5f, this.obstacle);
 
+        return hit.collider;
+    }
+}
