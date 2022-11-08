d050401c3087a928931d9288d79e891ad2ed6f05
Pholith <vincent22062@gmail.com>
2022-11-08 11:25:18 -0500

    Petites modifs  éléments

--- GodAlchemy/Assets/Script/Manager/ElementsManager.cs
+++ GodAlchemy/Assets/Script/Manager/ElementsManager.cs
@@ -9,6 +9,10 @@ public class ElementsManager : BaseManager<ElementsManager>
     {
     }
 
+    [SerializeField]
+    private Vector3Int particleSystemOffsets = new(0, 5, 0);
+    [SerializeField]
+    private float timeInSecondAfterParticleStart = 2;
 
 
     public void Fire()
@@ -24,14 +28,34 @@ public class ElementsManager : BaseManager<ElementsManager>
     }
 
     [SerializeField]
+    private ParticleSystem airParticlePrefab;
+    public void Air()
+    {
+        airParticlePrefab.gameObject.Instantiate(GameManager.GridManager.GetMouseGridPos());
+    }
+
+    [SerializeField]
     private TileBase dirtTile;
+    [SerializeField]
+    private ParticleSystem dirtParticlePrefab;
     public void Dirt()
     {
+<<<<<<< Updated upstream
         GameManager.GridManager.SetTilesOnMouseInRange(dirtTile, 3);
+=======
+<<<<<<< Updated upstream
+        GameManager.GridManager.SetTilesOnMouseInRange(dirtTile, 4);
+=======
+        dirtParticlePrefab.gameObject.Instantiate(GameManager.GridManager.GetMouseGridPos() + particleSystemOffsets);
+        var mousePos = GameManager.GridManager.GetMouseGridPos();
+        new GameTimer(timeInSecondAfterParticleStart, () => GameManager.GridManager.SetTileInRange(dirtTile, mousePos.ToVector3Int(), 4));
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     }
 
 
     [SerializeField]
+<<<<<<< Updated upstream
     private GameObject waterPrefab;
     public void Water()
     {
@@ -42,11 +66,33 @@ public class ElementsManager : BaseManager<ElementsManager>
     }
 
     [SerializeField]
+=======
+<<<<<<< Updated upstream
+=======
+    private ParticleSystem waterParticlePrefab;
+    public void Water()
+    {
+        waterParticlePrefab.gameObject.Instantiate(GameManager.GridManager.GetMouseGridPos() + particleSystemOffsets);
+        var mousePos = GameManager.GridManager.GetMouseGridPos();
+        new GameTimer(timeInSecondAfterParticleStart, () => GameManager.GridManager.SetTileInRange(null, mousePos.ToVector3Int(), 4));
+    }
+
+    [SerializeField]
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     private GameObject lightningPrefab;
     public void Lightning()
     {
         GameObject lightning = Instantiate(lightningPrefab);
+<<<<<<< Updated upstream
         lightning.transform.position = GameManager.GridManager.GetMouseGridPos(); ;
+=======
+<<<<<<< Updated upstream
+        lightning.transform.position = mousePos;
+=======
+        lightning.transform.position = GameManager.GridManager.GetMouseGridPos();
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
         var animator = lightning.GetComponent<Animator>();
         // Mettre le feu et particules de feu
     }
