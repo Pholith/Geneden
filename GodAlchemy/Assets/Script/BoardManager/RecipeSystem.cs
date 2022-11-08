e5fe54503c66f13be97e5070480ff0f852002dfa
bagurb <bastien.bagur@edu.esiee.fr>
2022-11-07 20:58:39 -0500

    Fix RecipeSystem

--- GodAlchemy/Assets/Script/BoardManager/RecipeSystem.cs
+++ GodAlchemy/Assets/Script/BoardManager/RecipeSystem.cs
@@ -36,55 +36,38 @@ public class RecipeSystem : MonoBehaviour
         }
     }
 
-    private bool IsPrimaryElement(ElementScriptableObject element)
-    {
-        if (element.name == "Feu" || element.name == "Eau" || element.name == "Air" || element.name == "Terre")
-            return true;
-        return false;
-    }
-
     public void AddRecipeToCraft(ElementScriptableObject element)
     {
         ElementScriptableObject[] elementForCraft = { element.ElementToCraft1, element.ElementToCraft2 };
-        if(element.DifficultyToCraft > 2)
-        {
             foreach(ElementScriptableObject craftElement in elementForCraft)
             {
                 if (!CheckPlayerElement(craftElement))
-                    return;
+                {
+                    if (craftElement.IsPrimaryElement)
+                    {
+                        craftSystem.AddRessource(craftElement);
+                    }
+                }
+                else
+                {
+                    AddPlayerElement(craftElement);
+                }
             }
-            foreach(ElementScriptableObject craftElement in elementForCraft)
-            {
-                AddPlayerElement(craftElement);
-            }
-        }
-        else
-        {
-            craftSystem.AddRessource(element.ElementToCraft1);
-            craftSystem.AddRessource(element.ElementToCraft2);
-        }    
     }
 
     private bool CheckPlayerElement(ElementScriptableObject element)
     {
-        if (IsPrimaryElement(element))
-        {
-            return true;
-        }
-        else
-        {
             ItemSlot _playerElement = playerInventory.SearchElement(element);
             if (_playerElement != null)
             {
                 return true;
             }
-        }
         return false;
     }
 
     private void AddPlayerElement(ElementScriptableObject element)
     {
-        if (IsPrimaryElement(element))
+        if (element.IsPrimaryElement)
         {
             craftSystem.AddRessource(element);
         }
