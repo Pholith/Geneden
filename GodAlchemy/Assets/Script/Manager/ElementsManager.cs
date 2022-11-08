a1c4641ff6b2c95a5fcef8558163c9fd3aa771bf
Pholith <vincent22062@gmail.com>
2022-11-08 11:48:27 -0500

    Merge de mes petits ajouts

--- GodAlchemy/Assets/Script/Manager/ElementsManager.cs
+++ GodAlchemy/Assets/Script/Manager/ElementsManager.cs
@@ -9,6 +9,10 @@ public class ElementsManager : BaseManager<ElementsManager>
     {
     }
 
+    [SerializeField]
+    private Vector3Int particleSystemOffsets = new(0, 5, 0);
+    [SerializeField]
+    private float timeInSecondAfterParticleStart = 2;
 
 
     public void Fire()
@@ -24,21 +28,31 @@ public class ElementsManager : BaseManager<ElementsManager>
     }
 
     [SerializeField]
+    private ParticleSystem airParticlePrefab;
+    public void Air()
+    {
+        airParticlePrefab.gameObject.Instantiate(GameManager.GridManager.GetMouseGridPos());
+    }
+
+    [SerializeField]
     private TileBase dirtTile;
+    [SerializeField]
+    private ParticleSystem dirtParticlePrefab;
     public void Dirt()
     {
-        GameManager.GridManager.SetTilesOnMouseInRange(dirtTile, 3);
+        dirtParticlePrefab.gameObject.Instantiate(GameManager.GridManager.GetMouseGridPos() + particleSystemOffsets);
+        var mousePos = GameManager.GridManager.GetMouseGridPos();
+        new GameTimer(timeInSecondAfterParticleStart, () => GameManager.GridManager.SetTileInRange(dirtTile, mousePos.ToVector3Int(), 4));
     }
 
 
     [SerializeField]
-    private GameObject waterPrefab;
+    private ParticleSystem waterParticlePrefab;
     public void Water()
     {
-        var water = Instantiate(waterPrefab);
-        water.transform.position = GameManager.GridManager.GetMouseGridPos() + new Vector3Int(0, 5, 0);
+        waterParticlePrefab.gameObject.Instantiate(GameManager.GridManager.GetMouseGridPos() + particleSystemOffsets);
         var mousePos = GameManager.GridManager.GetMouseGridPos();
-        new GameTimer(2, () => GameManager.GridManager.SetTileInRange(null, mousePos.ToVector3Int(), 4));
+        new GameTimer(timeInSecondAfterParticleStart, () => GameManager.GridManager.SetTileInRange(null, mousePos.ToVector3Int(), 4));
     }
 
     [SerializeField]
@@ -46,7 +60,7 @@ public class ElementsManager : BaseManager<ElementsManager>
     public void Lightning()
     {
         GameObject lightning = Instantiate(lightningPrefab);
-        lightning.transform.position = GameManager.GridManager.GetMouseGridPos(); ;
+        lightning.transform.position = GameManager.GridManager.GetMouseGridPos();
         var animator = lightning.GetComponent<Animator>();
         // Mettre le feu et particules de feu
     }
